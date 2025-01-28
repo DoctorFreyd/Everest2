36ba0f675a0ff94f2226192a8adbba6f0256c302
Winston Smith <winstonnn.smith.1984@gmail.com>
2025-01-28 22:19:18 +0300

    firs init

---
+++ src/app/pages/booking/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Booking() {
+    return(
+        <div>
+            <h1>Booking page</h1>
+        </div>
+    );
+}
---
+++ src/app/pages/shop/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Shop() {
+    return(
+        <div>
+            <h1>Shop page</h1>
+        </div>
+    );
+}
